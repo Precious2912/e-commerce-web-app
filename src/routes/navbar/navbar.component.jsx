47d580de3fcd8c6084b178b82494b00a0a3a7630
Precious2912 <preciousovat@gmail.com>
2022-12-01 14:18:24 +0100

    added cart-icon and cart-dropdown

--- src/routes/navbar/navbar.component.jsx
+++ src/routes/navbar/navbar.component.jsx
@@ -2,11 +2,15 @@ import { useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import "./navbar.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 const Navigation = () => {
   const { currentUser, setCurentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   const signOutHandler = async () => {
     await signOutUser();
@@ -34,7 +38,9 @@ const Navigation = () => {
               SIGNIN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </>
